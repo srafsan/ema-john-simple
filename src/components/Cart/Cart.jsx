485d0fedfbb2ac3ce9250398daf83b57e6d79026
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-03-28 10:40:43 +0600

    Added the cart component with price calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,19 +5,24 @@ import "./Cart.css";
 const Cart = (props) => {
     const { cart } = props;
 
-    let total = 0;
+    let totalPrice = 0,
+        totalShipping = 0;
     for (const product of cart) {
-        total = total + product.price;
+        totalPrice += product.price;
+        totalShipping += product.shipping;
     }
+
+    const tax = totalPrice * 0.07,
+        grandTotal = totalPrice + totalShipping + tax;
 
     return (
         <div className="cart">
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h6>Grand Total: </h6>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
